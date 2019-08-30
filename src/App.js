9f91b05728f1a62cfaa2bb9532e89d49d2e0e0b8
github username <dallasbille@gmail.com>
2019-08-30 15:35:28 -0400

    added user location marker to map

--- src/App.js
+++ src/App.js
@@ -1,45 +1,92 @@
-import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl'
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "./App.css";
 
 class App extends Component {
-        state = {
-            viewport: {
-              width: "100vw",
-              height: "100vh",
-              latitude: 42.430472,
-              longitude: -123.334102,
-              zoom: 16
-          },
-        };
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      latitude: 42.430472,
+      longitude: -123.334102,
+      zoom: 16
+    },
+    stations: {},
+    userLocation: {}
+  };
 
-        setUserLocation = () => {
-            navigator.geolocation.getCurrentPosition(position => {
-                let newViewport = {
-                    height: "100vh",
-                    width: "100vw",
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    zoom: 10
-                }
-                this.setState({
-                    viewport: newViewport
-                })
-            })
-        }
+  setUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      let setUserLocation = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      };
+      let newViewport = {
+        height: "100vh",
+        width: "100vw",
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 10
+      };
+      this.setState({
+        viewport: newViewport,
+        userLocation: setUserLocation
+      });
+    });
+  };
 
-render(){
-  return (
-    <div className="App">
-      <button onClick={this.setUserLocation} >My Location</button>
-      <div className="map">
-        <ReactMapGL {...this.state.viewport} mapStyle="mapbox://styles/mapbox/outdoors-v11" onViewportChange={(viewport => this.setState({viewport}))} mapboxApiAccessToken="pk.eyJ1IjoiZGFsbGFzYmlsbGUiLCJhIjoiY2p6OHR1aGhoMDZnZDNjbXB2ZWZlcXFudCJ9.gjjYkOkTtA-Qe1jhbvF2gQ">
-        </ReactMapGL>
+  // insertMarkerComp = () => {
+  //   return (
+  //     <Marker
+  //       latitude={this.state.userLocation.latitude}
+  //       longitude={this.state.userLocation.longitude}
+  //     >
+  //       <div>MARKER</div>
+  //     </Marker>
+  //   );
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setUserLocation}>My Location</button>
+        <div className="map">
+          <ReactMapGL
+            {...this.state.viewport}
+            mapStyle="mapbox://styles/mapbox/outdoors-v11"
+            onViewportChange={viewport => this.setState({ viewport })}
+            mapboxApiAccessToken="pk.eyJ1IjoiZGFsbGFzYmlsbGUiLCJhIjoiY2p6OHR1aGhoMDZnZDNjbXB2ZWZlcXFudCJ9.gjjYkOkTtA-Qe1jhbvF2gQ"
+          >
+            {Object.keys(this.state.userLocation).length !== 0 ? (
+              <Marker
+                latitude={this.state.userLocation.lat}
+                longitude={this.state.userLocation.long}
+              >
+                <img className="location-icon" src="location-icon.svg" />
+              </Marker>
+            ) : (
+              <div></div>
+            )}
+          </ReactMapGL>
         </div>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
 export default App;
+
+// fetchStationAPI = () => {
+//   fetch(
+//     `https://data.ny.gov/resource/nyctransitsubwayentrance-and-exit.json?`
+//   )
+//     .then(res => res.json())
+//     .then(stations => {
+//       this.setState({
+//         stations: stations
+//       });
+//     });
+// };
+// componentDidMount() {
+//   this.fetchStationAPI();
+// }
